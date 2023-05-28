888b661cf1c52e011a174e30a7f7bc60041ce810
Gaurav Chitwaan <gaurav198.rr@gmail.com>
2023-05-29 01:26:19 +0530

    refresh token logic implemented

--- routes/auth.js
+++ routes/auth.js
@@ -23,11 +23,32 @@ router.post('/login', async (req, res) => {
     if(!user || !(await bcrypt.compare(password, user.password))) {
       throw new Error('Invalid email or password');
     }
-    const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET);
-    res.send({token});
+    const accessToken = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {expiresIn: '15m'});
+    const refreshToken = jwt.sign({userId: user.id}, process.env.REFRESH_TOKEN_SECRET);
+    user.refreshToken = refreshToken;
+    await user.save();
+    res.send({accessToken, refreshToken});
   } catch (err) {
     res.status(401).send(err.message);
   }
+});
+
+router.post('/refresh', async (req, res) => {
+  const {refreshToken} = req.body;
+  if(!refreshToken) {
+    return res.sendStatus(401);
+  }
+  const user = await User.findOne({refreshToken});
+  if(!user) {
+    return res.sendStatus(403);
+  }
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if(err) {
+      return res.sendStatus(403);
+    }
+    const accessToken = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {expiresIn: '15m'});
+    res.send({accessToken});
+  });
 });
 
 module.exports = router;
